e0368a9b45e8325492c843ae72279ab76f83f744
Aseiide <eiseiide@gmail.com>
2021-10-24 13:01:10 +0900

    TODOアプリを作成

---
+++ index.js
@@ -0,0 +1,33 @@
+new Vue ({
+  el: "#app",
+  data: {
+    newItem: "",
+    todos: []
+  },
+  watch: {
+    todos: {
+      handler: function() {
+        localStorage.setItem("todos", JSON.stringify(this.todos));
+      },
+      deep: true
+    }
+  },
+  mounted: function() {
+    this.todos = JSON.parse(localStorage.getItem("todos")) || [];
+  },
+  methods: {
+    addItem: function() {
+      const item = {
+        title: this.newItem,
+        isDone: false
+      }
+      this.todos.push(item)
+      this.newItem = ""
+    },
+    deleteItem: function(index) {
+      if (confirm("are you sure?")) {
+        this.todos.splice(index, 1)
+      }
+    }
+  }
+})
